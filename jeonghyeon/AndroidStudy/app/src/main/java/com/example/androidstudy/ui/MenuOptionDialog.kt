c6094215cafa4bd5f5c60132076285773e5d1636
박정현 <qgam12123@gmail.com>
2023-10-13 16:21:54 +0900

    옵션Dialog하단 배치

--- jeonghyeon/AndroidStudy/app/src/main/java/com/example/androidstudy/ui/MenuOptionDialog.kt
+++ jeonghyeon/AndroidStudy/app/src/main/java/com/example/androidstudy/ui/MenuOptionDialog.kt
@@ -1,25 +1,28 @@
 package com.example.androidstudy.ui
 
+import android.annotation.SuppressLint
 import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.DialogFragment
-import com.example.androidstudy.data.Americano
+import com.example.androidstudy.R
 import com.example.androidstudy.data.CoffeeOption
 import com.example.androidstudy.data.MenuData
 import com.example.androidstudy.data.Temperature
 import com.example.androidstudy.databinding.DialogMenuOptionBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class MenuOptionDialog(
     private val isAmericano: Boolean,
     private var dismissResult : ((MenuData)-> Unit)
-    ): DialogFragment() {
+    ): BottomSheetDialogFragment() {
 
     /**
      * 사용자가 선택한 메뉴 정보에 대한 Key값 (String 이름)
@@ -49,7 +52,18 @@ class MenuOptionDialog(
     }
 
     /**
-     *시작 시 Dialog 설정 ( dialog 취소 설정 )
+     * 스타일 설정, arguments 받기
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {bundle ->
+            menu = bundle.getString(MENU_KEY) ?: ""
+            Log.d("wd",menu)
+        }
+    }
+
+    /**
+     *시작 시 Dialog 설정 ( dialog 취소 설정, 레이아웃, 크기 위치 조정)
      */
     override fun onStart() {
         super.onStart()
@@ -57,14 +71,14 @@ class MenuOptionDialog(
             isCancelable = true
             setCanceledOnTouchOutside(true)
         }
-    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {bundle ->
-            menu = bundle.getString(MENU_KEY) ?: ""
-            Log.d("wd",menu)
-        }
+        /**
+         * 레이아웃 크기, 위치 조정
+         */
+        val width = ViewGroup.LayoutParams.MATCH_PARENT
+        val height = ViewGroup.LayoutParams.WRAP_CONTENT
+        dialog?.window?.setLayout(width, height)
+        dialog?.window?.setGravity(Gravity.BOTTOM)
     }
 
     /**
